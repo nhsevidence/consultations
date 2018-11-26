0a19414053c99e5d6ed99871eff46adc196f64a8
John Williams <jcwilliams@gmail.com>
2018-11-26 16:27:03 +0000

    EC-612 - adding more tests

--- Comments/ClientApp/src/components/HeaderButton/HeaderButton.js
+++ Comments/ClientApp/src/components/HeaderButton/HeaderButton.js
@@ -4,23 +4,20 @@ import React, {Fragment, PureComponent} from "react";
 import Moment from "react-moment";
 
 type PropsType = {
-	title: string,
-	endDate?: any,
-	match?: any,
-	subtitle1?: string,
-	subtitle2?: string,
-	consultationState?: {
-		endDate: string,
-		consultationIsOpen: boolean,
-		consultationHasNotStartedYet: ?boolean,
-	},
+	isAuthorised: boolean,
+	responseCount?: number,
 }
 
 export class HeaderButton extends PureComponent<PropsType> {
 
 	render() {
 		return (
-			<div>hello</div>	
+			<div>
+				{this.props.isAuthorised && this.props.responseCount > 0 ?
+					<button>Hello
+					</button>
+					: null}
+			</div>
 		);
 	}
 
--- Comments/ClientApp/src/components/HeaderButton/__tests__/HeaderButton.test.js
+++ Comments/ClientApp/src/components/HeaderButton/__tests__/HeaderButton.test.js
@@ -13,5 +13,21 @@ describe("[ClientApp] ", () => {
 			);
 		  	expect(wrapper.find("button").length).toEqual(1);
 		});
+
+		it("button links to the review page", () => {
+		
+			const wrapper = shallow(
+				<HeaderButton isAuthorised={true} responseCount={1} />
+			);
+		  	expect(wrapper.find("button").todo).toEqual(1);
+		});
+
+		it("button is not found when user is not authenticated and there are responses", () => {
+		
+			const wrapper = shallow(
+				<HeaderButton isAuthorised={false} responseCount={1} />
+			);
+		  	expect(wrapper.find("button").length).toEqual(0);
+		});
 	});
 });
